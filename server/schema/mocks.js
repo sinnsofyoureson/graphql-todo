b3ac6b4350b5f071ddb91a15b472200e00a8c6da
Rinat Moskvin <moskvin@mercurydevelopment.com>
2022-02-14 01:35:24 +0400

    Added getDirector API

---
+++ server/schema/mocks.js
@@ -0,0 +1,15 @@
+const movies = [
+  { id: "1", name: "Pulp Fiction", genre: "Crime", directorId: "1" },
+  { id: "2", name: "1984", genre: "Sci-Fi", directorId: "2" },
+  { id: 3, name: "V for vendetta", genre: "Sci-Fi-Triller", directorId: "3" },
+  { id: 4, name: "Snatch", genre: "Crime-Comedy", directorId: "4" },
+];
+
+const directors = [
+  { id: "1", name: "Quentin Tarantino", age: 55 },
+  { id: "2", name: "Michael Radford", age: 72 },
+  { id: "3", name: "James McTeigue", age: 51 },
+  { id: "4", name: "Guy Ritchie", age: 50 },
+];
+
+module.exports = { movies, directors };
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,33 @@
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = require("graphql");
+const { movies, directors } = require("./mocks");
 
-const GetMovieType = new GraphQLObjectType({
+const MovieType = new GraphQLObjectType({
   name: "Movie",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    director: {
+      type: DirectorType,
+      resolve(parent, _args) {
+        return directors.find((director) => director.id == parent.id);
+      },
+    },
+  }),
+});
+
+const DirectorType = new GraphQLObjectType({
+  name: "Director",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -13,9 +35,18 @@ const Query = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
     getMovie: {
-      type: GetMovieType,
-      args: { id: { type: GraphQLString } },
-      resolve(parent, args) {},
+      type: MovieType,
+      args: { id: { type: GraphQLID } },
+      resolve(_parent, args) {
+        return movies.find((movie) => movie.id == args.id);
+      },
+    },
+    getDirector: {
+      type: DirectorType,
+      args: { id: { type: GraphQLID } },
+      resolve(_parent, args) {
+        return directors.find((director) => director.id == args.id);
+      },
     },
   },
 });
