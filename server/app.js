42dcd14c7382f6aaa65e18a9584e9375d8202a4c
Rinat Moskvin <moskvin@mercurydevelopment.com>
2022-02-16 00:58:42 +0400

    Updated schema to work with MongoDB

--- server/app.js
+++ server/app.js
@@ -3,13 +3,24 @@ const jsonGraphqlExpress = require("json-graphql-server");
 const { todos } = require("./schema/mocks");
 // const { graphqlHTTP } = require("express-graphql");
 // const schema = require("./schema/schema");
+// const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 4000;
 
-// app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
-
 const data = { todos };
+
+/*
+ * Use code below if you have configured MongoDB
+ *
+ * mongoose.connect("mongodb://", { useMongoClient: true });
+ * app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
+ * const dbConnection = mongoose.connection;
+ * dbConnection.on("error", (error) => console.log("DB connection error", error));
+ * dbConnection.once("open", () => "DB connection established!");
+ */
+
+// Use this for fake GraphQl database
 app.use("/graphql", jsonGraphqlExpress.default(data));
 
 app.listen(PORT, (error) => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,7 +6,9 @@ const {
   GraphQLInt,
   GraphQLList,
 } = require("graphql");
-const { movies, directors } = require("./mocks");
+const Movies = require("../models/movie");
+const Directors = require("../models/director");
+// const { movies, directors } = require("./mocks");
 
 const MovieType = new GraphQLObjectType({
   name: "Movie",
@@ -17,7 +19,8 @@ const MovieType = new GraphQLObjectType({
     director: {
       type: DirectorType,
       resolve(parent, _args) {
-        return directors.find((director) => director.id == parent.id);
+        // return directors.find((director) => director.id == parent.id);
+        return Directors.findById(parent.directorId);
       },
     },
   }),
@@ -31,8 +34,9 @@ const DirectorType = new GraphQLObjectType({
     age: { type: GraphQLInt },
     movies: {
       type: new GraphQLList(MovieType),
-      resolve(parent, ars) {
-        return movies.filter((movie) => movie.directorId === parent.id);
+      resolve(parent, _args) {
+        // return movies.filter((movie) => movie.directorId === parent.id);
+        return Movies.find({ directorId: parent.id });
       },
     },
   }),
@@ -45,26 +49,30 @@ const Query = new GraphQLObjectType({
       type: MovieType,
       args: { id: { type: GraphQLID } },
       resolve(_parent, args) {
-        return movies.find((movie) => movie.id == args.id);
+        // return movies.find((movie) => movie.id == args.id);
+        return Movies.findById(args.id);
       },
     },
     getMoviesList: {
       type: new GraphQLList(MovieType),
       resolve(_parent, _args) {
-        return movies;
+        // return movies;
+        return Movies.find({});
       },
     },
     getDirector: {
       type: DirectorType,
       args: { id: { type: GraphQLID } },
       resolve(_parent, args) {
-        return directors.find((director) => director.id == args.id);
+        // return directors.find((director) => director.id == args.id);
+        return Directors.findById(args.id);
       },
     },
     getDirectorsList: {
       type: new GraphQLList(DirectorType),
       resolve(_parent, _args) {
-        return directors;
+        // return directors;
+        return Directors.find({});
       },
     },
   },
